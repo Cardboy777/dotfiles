00c65088296e7dcb67b83aa0adb4f5c56a8ae746
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2024-01-26 00:40:22 -0500

    nvim Cut/delete remamps

--- config/nvim/lua/v1/core/keymaps.lua
+++ config/nvim/lua/v1/core/keymaps.lua
@@ -3,3 +3,12 @@ vim.keymap.set('n', '<leader>ga', function()
 end, { desc = '[G]it [A]dd' })
 
 vim.keymap.set('n', '<leader>gp', function() end, { desc = '[G]it [P]ush' })
+
+-- Make Deleting actually delete
+vim.keymap.set('n', 'd', '"_d', { desc = 'Delete [N]umber [D]irection' })
+vim.keymap.set('n', 'dd', '"_dd', { desc = 'Delete line' })
+vim.keymap.set('v', 'd', '"_d', { desc = 'Delete Selected Text' })
+-- Make cut behave like default delete
+vim.keymap.set('n', 'c', 'd', { desc = 'Cut [N]umber [D]irection' })
+vim.keymap.set('n', 'cc', 'dd', { desc = 'Cut line' })
+vim.keymap.set('v', 'c', 'd', { desc = 'Cut Selected Text' })
