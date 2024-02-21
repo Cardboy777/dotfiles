bfc80f8d1fe41e2db4cccb692b46bac06eba0892
Reid Kippenbrock <rkippenbrock@onboardmeetings.com>
2024-02-21 13:51:34 -0500

    add clangd

--- config/nvim/lua/v1/lsp/config.lua
+++ config/nvim/lua/v1/lsp/config.lua
@@ -149,6 +149,7 @@ require('mason-tool-installer').setup({
   -- a list of all tools you want to ensure are installed upon
   -- start
   ensure_installed = {
+    'clangd',
     'bash-language-server',
     'editorconfig-checker',
     'eslint_d',
