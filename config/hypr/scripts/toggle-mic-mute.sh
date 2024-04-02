646b2b82018fa26751da94fbd3b465d1226074fe
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2024-04-02 15:20:52 -0400

    add hyprland config

--- config/hypr/scripts/toggle-mic-mute.sh
+++ config/hypr/scripts/toggle-mic-mute.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # this scripts finds the wireplumber device id for the microphone and toggles the mute state
 
-device_name="Scarlett 2i2 3rd Gen Input 1";
+device_name="Scarlett 2i2 3rd Gen Input 1 Mic/Inst/Line";
 
 # get the wireplumber device id for the microphone
 device_id=$(wpctl status | grep "$device_name" | grep -oEm 1 '[[:digit:]]+\.' | cut -d'.' -f1 | head -n 1);
 
+echo "Device Id: $device_id";
+
 wpctl set-mute "$device_id" toggle;
 
 volume=$(wpctl get-volume "$device_id");
+
+echo "'$device_name' Volume is $volume";
 
 audio="$HOME/Music/_discord/unmute.mp3"
 if [[ $volume == *"[MUTED]"* ]]; then
