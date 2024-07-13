cb0d67d81c4110aafb01c494680a1527d3cc946d
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2024-07-13 14:20:54 -0400

    Themes setup script

--- installThemes.sh
+++ installThemes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-paru -S gtk-engine-murrine sassc papirus-icon-theme bibata-cursor-git
+paru -S gtk-engine-murrine sassc papirus-icon-theme bibata-cursor-git lxappearance nwg-look
 
 INSTALL_DIR=~/Repositories/Tokyonight-GTK-Theme
 
@@ -10,10 +10,12 @@ git clone https://github.com/Fausto-Korpsvart/Tokyonight-GTK-Theme.git $INSTALL_
 
 chmod +x $INSTALL_DIR/themes/install.sh
 
-$INSTALL_DIR/themes/install.sh -c dark -l --tweaks moon float
+$INSTALL_DIR/themes/install.sh -c -s compact dark -l --tweaks moon float
 
 sudo flatpak override --filesystem="$HOME/.themes"
 sudo flatpak override --filesystem="$HOME/.icons"
 sudo flatpak override --filesystem=xdg-config/gtk-4.0
 sudo flatpak override --env=GTK_THEME=Tokyonight-Dark-Moon
 flatpak override --user --filesystem=xdg-config/gtk-4.0
+
+echo "Configure with commands: 'lxappearance' and 'nwg-look'"
