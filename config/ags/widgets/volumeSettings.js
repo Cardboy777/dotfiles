dddc2ba983567349972f9b8fc4cce18c35378809
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2024-07-14 20:28:33 -0400

    use wayland volume control

--- config/ags/widgets/volumeSettings.js
+++ config/ags/widgets/volumeSettings.js
@@ -4,7 +4,8 @@ export function VolumeSettings() {
   return Widget.Button({
     class_name: "volume-settings",
     tooltipMarkup: "L: Control\nR: Helvum\nM:EasyEffects",
-    on_primary_click: () => Utils.execAsync(["pavucontrol"]),
+    on_primary_click: () =>
+      Utils.execAsync(["flatpak", "run", "com.saivert.pwvucontrol"]),
     on_secondary_click: () => Utils.execAsync(["helvum"]),
     on_middle_click: () => Utils.execAsync(["easyeffects"]),
     child: Widget.Icon({
