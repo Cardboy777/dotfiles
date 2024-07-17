e775a61f5a0579387a9c62bce18411ee562087b4
Reid Kippenbrock <22593559+Cardboy777@users.noreply.github.com>
2024-07-17 12:20:40 -0400

    fix server configs

--- config/nvim/lua/v1/lsp/server_configurations/tsserver.lua
+++ config/nvim/lua/v1/lsp/server_configurations/tsserver.lua
@@ -1,9 +1,11 @@
+local language_server_location = require('mason-registry').get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
+
 return {
   init_options = {
     plugins = {
       {
         name = '@vue/typescript-plugin',
-        location = require('mason-registry').get_package('vue-language-server'):get_install_path(),
+        location = language_server_location,
         languages = { 'vue' },
       },
     },
