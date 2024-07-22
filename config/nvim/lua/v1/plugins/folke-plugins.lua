3856ac691ce089eff852629c2d00f4c67d71d7ca
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2024-07-22 00:44:35 -0400

    nvim plugins

---
+++ config/nvim/lua/v1/plugins/folke-plugins.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {},
+  },
+  {
+    'folke/which-key.nvim',
+    dependencies = {
+      { 'echasnovski/mini.nvim', version = false },
+      'nvim-web-devicons',
+    },
+    opts = {},
+  },
+  {
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {},
+  },
+}
---
+++ config/nvim/lua/v1/plugins/tpope-plugins.lua
@@ -0,0 +1,27 @@
+return {
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+  'tpope/vim-eunuch',
+  'tpope/vim-vinegar',
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+}
