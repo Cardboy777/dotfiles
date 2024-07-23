ada6326912b3b48c513cfaf5ec141ba9e8a2a9c9
Reid Kippenbrock <22593559+Cardboy777@users.noreply.github.com>
2024-07-23 16:09:49 -0400

    Fix some Formatting and theming

--- config/nvim/lua/v1/plugins/theme.lua
+++ config/nvim/lua/v1/plugins/theme.lua
@@ -1,6 +1,13 @@
 return {
   'folke/tokyonight.nvim',
   opts = {},
+  -- dependencies = {
+  --   {
+  --     'HiPhish/rainbow-delimters.nvim',
+  --     url = 'https://gitlab.com/HiPhish/rainbow-delimiters.nvim',
+  --     dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  --   },
+  -- },
   config = function()
     require('tokyonight').setup({
       style = 'moon',
@@ -20,5 +27,15 @@ return {
       end,
     })
     vim.cmd([[colorscheme tokyonight-moon]])
+
+    -- local colors = require('tokyonight.colors').setup()
+    --
+    -- vim.api.nvim_set_hl(0, 'RainbowDelimiterRed', { fg = colors.red })
+    -- vim.api.nvim_set_hl(0, 'RainbowDelimiterYellow', { fg = colors.yellow })
+    -- vim.api.nvim_set_hl(0, 'RainbowDelimiterBlue', { fg = colors.blue })
+    -- vim.api.nvim_set_hl(0, 'RainbowDelimiterOrange', { fg = colors.orange })
+    -- vim.api.nvim_set_hl(0, 'RainbowDelimiterGreen', { fg = colors.green })
+    -- vim.api.nvim_set_hl(0, 'RainbowDelimiterViolet', { fg = colors.purple })
+    -- vim.api.nvim_set_hl(0, 'RainbowDelimiterCyan', { fg = colors.cyan })
   end,
 }
