a0ec9df354a5ce5dfa2f8222f9bd57664f3add95
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2024-07-31 02:23:16 -0400

    add more packages - fix scripts

--- config/hypr/scripts/toggle-mic-mute.sh
+++ config/hypr/scripts/toggle-mic-mute.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # this scripts finds the wireplumber device id for the microphone and toggles the mute state
 
-device_name="Scarlett 2i2 3rd Gen Input 1 Mic/Inst/Line";
+device_name="Scarlett 2i2 3rd Gen Input 1 Mic/Inst/Line"
 
 # get the wireplumber device id for the microphone
-device_id=$(wpctl status | grep "$device_name" | grep -oEm 1 '[[:digit:]]+\.' | cut -d'.' -f1 | head -n 1);
+device_id=$(wpctl status | grep "$device_name" | grep -oEm 1 '[[:digit:]]+\.' | cut -d'.' -f1 | head -n 1)
 
-echo "Device Id: $device_id";
+echo "Device Id: $device_id"
 
-wpctl set-mute "$device_id" toggle;
+wpctl set-mute "$device_id" toggle
 
-volume=$(wpctl get-volume "$device_id");
+volume=$(wpctl get-volume "$device_id")
 
-echo "'$device_name' Volume is $volume";
+echo "'$device_name' Volume is $volume"
 
 audio="$HOME/Music/_discord/unmute.mp3"
 if [[ $volume == *"[MUTED]"* ]]; then
@@ -21,4 +21,4 @@ fi
 
 pkill -RTMIN+8 waybar
 
-play "$audio" trim 0 1 vol 0.15
+ffplay -v 0 -volume 30 -nodisp -autoexit "$audio"
