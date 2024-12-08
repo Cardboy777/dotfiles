88d87eb0b2e1ca881542081861468727d583bfec
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2024-12-08 01:46:33 -0500

    Mess with minuet a bit

---
+++ config/nvim/lua/v1/plugins/minuet-ai.lua
@@ -0,0 +1,14 @@
+return {
+  --   {
+  --     'milanglacier/minuet-ai.nvim',
+  --     depends = {
+  --       'nvim-lua/plenary.nvim',
+  --       'hrsh7th/nvim-cmp',
+  --     },
+  --     config = function()
+  --       require('minuet').setup({
+  --         -- Your configuration options here
+  --       })
+  --     end,
+  --   },
+}
