af5debd3ad86fb4cd6fdb67bbe98c2b8e4125868
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2024-12-21 01:03:05 -0500

    fix protonvpn tray icon

--- config/ags/widget/SystemTray.tsx
+++ config/ags/widget/SystemTray.tsx
@@ -1,9 +1,12 @@
-import { App } from "astal/gtk3"
-import { bind } from "astal"
-import { Gdk } from "astal/gtk3"
+import { bind, Variable } from "astal"
+import { App, Gdk } from "astal/gtk3"
 import Tray from "gi://AstalTray"
 
 const excludeList = ["Xwayland Video Bridge_pipewireToXProxy"];
+
+const trayIconOverride: Record<string, string> = {
+    'proton-vpn-app': 'proton-vpn-logo'
+} as const;
 
 export function SystemTray() {
     const tray = Tray.get_default()
@@ -12,9 +15,11 @@ export function SystemTray() {
         {bind(tray, "items").as(items => items
             .filter(item => !excludeList.includes(item.id))
             .map(item => {
-                //console.log(item.id)
-                if (item.iconThemePath)
+                console.log(item.id, item.iconThemePath);
+                let overrideIcon = new Variable<string | undefined>(trayIconOverride[item.id])
+                if (item.iconThemePath) {
                     App.add_icons(item.iconThemePath)
+                }
 
                 const menu = item.create_menu()
 
@@ -24,7 +29,13 @@ export function SystemTray() {
                     onClickRelease={self => {
                         menu?.popup_at_widget(self, Gdk.Gravity.SOUTH, Gdk.Gravity.NORTH, null)
                     }}>
-                    <icon gIcon={bind(item, "gicon")} />
+                    {
+                        bind(overrideIcon).as(v =>
+                            v
+                                ? (<icon icon={v} />)
+                                : (<icon gIcon={bind(item, "gicon")} />)
+                        )
+                    }
                 </button>
             }))}
     </box>
