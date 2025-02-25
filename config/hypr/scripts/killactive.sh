4426ac1b3860d98c341c6ebfbecaaadb1ec83d66
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2025-02-25 16:00:29 -0500

    steam/vesktop wierdness

--- config/hypr/scripts/killactive.sh
+++ config/hypr/scripts/killactive.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$(hyprctl activewindow -j | jq -r ".class")" = "Steam" ]; then
-    xdotool getactivewindow windowunmap
-elif [ "$(hyprctl activewindow -j | jq -r ".class")" = "vesktop" ]; then
-    xdotool getactivewindow windowunmap
-else
-    hyprctl dispatch killactive ""
-fi
+# if [ "$(hyprctl activewindow -j | jq -r ".class")" = "Steam" ]; then
+#     xdotool getactivewindow windowunmap
+# elif [ "$(hyprctl activewindow -j | jq -r ".class")" = "vesktop" ]; then
+#     xdotool getactivewindow windowunmap
+# else
+hyprctl dispatch killactive ""
+# fi
