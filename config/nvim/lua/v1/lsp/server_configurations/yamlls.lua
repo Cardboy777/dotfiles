c56d9dd05c18f862944d6f5c0457751df391c25c
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2025-03-03 02:07:12 -0500

    fix lsp schema stores

--- config/nvim/lua/v1/lsp/server_configurations/yamlls.lua
+++ config/nvim/lua/v1/lsp/server_configurations/yamlls.lua
@@ -1,1 +1,18 @@
-return {}
+return {
+  settings = {
+    redhat = {
+      telemetry = { enabled = false },
+    },
+    yaml = {
+      format = {
+        enable = true,
+        printWidth = 120,
+      },
+      schemaStore = {
+        enable = false,
+        url = '',
+      },
+      schemas = require('schemastore').yaml.schemas(),
+    },
+  },
+}
