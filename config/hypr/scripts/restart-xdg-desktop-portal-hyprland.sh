c9cd23642a6a42c938d2e4c8128885c22255c6d9
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2025-04-29 03:00:28 -0400

    misc updates

--- config/hypr/scripts/restart-xdg-desktop-portal-hyprland.sh
+++ config/hypr/scripts/restart-xdg-desktop-portal-hyprland.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-sleep 4
-killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-wlr
-killall -e xdg-desktop-portal-gtk
-killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-hyprland &
-/usr/lib/xdg-desktop-portal-gtk &
-sleep 4
-/usr/lib/xdg-desktop-portal &
+
