3b82714cf80e55d1ef486920f4c990efc0cc6bed
Reid Kippenbrock <rkippenbrock@onboardmeetings.com>
2025-06-24 12:14:07 -0400

    c# debugging

--- config/nvim/lua/v2/plugins/debug.lua
+++ config/nvim/lua/v2/plugins/debug.lua
@@ -23,6 +23,7 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+    'GustavEikaas/easy-dotnet.nvim',
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
@@ -64,7 +65,7 @@ return {
     {
       '<leader>B',
       function()
-        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))
       end,
       desc = 'Debug: Set Breakpoint',
     },
@@ -78,10 +79,10 @@ return {
     },
   },
   config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+    local dap = require('dap')
+    local dapui = require('dapui')
 
-    require('mason-nvim-dap').setup {
+    require('mason-nvim-dap').setup({
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
       automatic_installation = true,
@@ -96,11 +97,11 @@ return {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
       },
-    }
+    })
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
+    dapui.setup({
       -- Set icons to characters that are more likely to work in every terminal.
       --    Feel free to remove or use ones that you like more! :)
       --    Don't feel like these are good choices.
@@ -118,7 +119,7 @@ return {
           disconnect = '⏏',
         },
       },
-    }
+    })
 
     -- Change breakpoint icons
     -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
@@ -137,12 +138,86 @@ return {
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     -- Install golang specific config
-    require('dap-go').setup {
+    require('dap-go').setup({
       delve = {
         -- On Windows delve must be run attached or it crashes.
         -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
+        detached = vim.fn.has('win32') == 0,
       },
-    }
+    })
+
+    -- C# setup
+    local function rebuild_project(co, path)
+      local spinner = require('easy-dotnet.ui-modules.spinner').new()
+      spinner:start_spinner('Building')
+      vim.fn.jobstart(string.format('dotnet build %s', path), {
+        on_exit = function(_, return_code)
+          if return_code == 0 then
+            spinner:stop_spinner('Built successfully')
+          else
+            spinner:stop_spinner('Build failed with exit code ' .. return_code, vim.log.levels.ERROR)
+            error('Build failed')
+          end
+          coroutine.resume(co)
+        end,
+      })
+      coroutine.yield()
+    end
+
+    local function file_exists(path)
+      local stat = vim.loop.fs_stat(path)
+      return stat and stat.type == 'file'
+    end
+
+    local dotnet = require('easy-dotnet')
+    local debug_dll = nil
+
+    local function ensure_dll()
+      if debug_dll ~= nil then
+        return debug_dll
+      end
+      local dll = dotnet.get_debug_dll()
+      debug_dll = dll
+      return dll
+    end
+    for _, value in ipairs({ 'cs', 'fsharp' }) do
+      dap.configurations[value] = {
+        {
+          type = 'coreclr',
+          name = 'Program',
+          request = 'launch',
+          env = function()
+            local dll = ensure_dll()
+            local vars = dotnet.get_environment_variables(dll.project_name, dll.absolute_project_path, false)
+            return vars or nil
+          end,
+          program = function()
+            local dll = ensure_dll()
+            local co = coroutine.running()
+            rebuild_project(co, dll.project_path)
+            if not file_exists(dll.target_path) then
+              error('Project has not been built, path: ' .. dll.target_path)
+            end
+            return dll.target_path
+          end,
+          cwd = function()
+            local dll = ensure_dll()
+            return dll.absolute_project_path
+          end,
+        },
+      }
+
+      dap.listeners.before['event_terminated']['easy-dotnet'] = function()
+        debug_dll = nil
+      end
+
+      dap.adapters.coreclr = {
+        type = 'executable',
+        command = 'netcoredbg',
+        args = { '--interpreter=vscode' },
+      }
+    end
+
+    dotnet.setup()
   end,
 }
