9e5d4ea90a876fa0adca4aab6cda830112814f7f
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2025-08-20 01:40:59 -0400

    add hosts alias

--- config/fish/bash.d/aliases.sh
+++ config/fish/bash.d/aliases.sh
@@ -11,16 +11,10 @@ alias firefox=brave
 
 alias neofetch=fastfetch
 
-alias hosts=cat /etc/hosts | grep "192\.168\.1\..*"
-
 # tmux
 alias tl='tmux ls'
 alias ta='tmux a -t'
 alias tk='~/.config/fish/bash.d/scripts/tmux-kill.sh'
 alias ts='~/.config/fish/bash.d/scripts/tmux-sessionizer.sh'
 
-shortcut() {
-    target=$1
-    destination=$2
-    ln -S -T "$target" "$destination"
-}
+alias hosts='cat /etc/hosts | grep "192\.168\.1\..*"'
