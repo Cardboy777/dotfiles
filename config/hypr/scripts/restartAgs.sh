709303f32ef149f265d727b79cca24fb03edafd7
Reid Kippenbrock <rkippenbrock@onboardmeetings.com>
2025-10-29 13:05:29 -0400

    fix ags

--- config/hypr/scripts/restartAgs.sh
+++ config/hypr/scripts/restartAgs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $(pgrep -x "gjs") != "" ]]; then
-    killall -KILL gjs
+	killall -KILL gjs
 fi
 
 mkdir -p "$XDG_STATE_HOME/ags"
 
-ags run >"$XDG_STATE_HOME/ags/ags.log" 2>&1
+ags run --gtk 3 >"$XDG_STATE_HOME/ags/ags.log" 2>&1
