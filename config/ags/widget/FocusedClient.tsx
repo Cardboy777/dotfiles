76a6f045d69fe04f182f1fa9e02812a98050ff8b
Cardboy777 <22593559+Cardboy777@users.noreply.github.com>
2025-11-21 01:59:01 -0500

    niri

--- config/ags/widget/FocusedClient.tsx
+++ config/ags/widget/FocusedClient.tsx
@@ -1,18 +1,18 @@
 import { bind } from "astal"
-import Hyprland from "gi://AstalHyprland"
+// import Hyprland from "gi://AstalHyprland"
 
 export function FocusedClient() {
-    const hypr = Hyprland.get_default()
-    const focused = bind(hypr, "focusedClient")
-
-    return <box
-        className="Focused"
-        visible={focused.as(Boolean)}>
-        <label
-			label={focused.as(c => c?.title ?? "")}
-			ellipsize="3"
-			max-width-chars="50"
-		/>
-    </box>
+	//   const hypr = Hyprland.get_default()
+	//   const focused = bind(hypr, "focusedClient")
+	//
+	//   return <box
+	//       className="Focused"
+	//       visible={focused.as(Boolean)}>
+	//       <label
+	// 	label={focused.as(c => c?.title ?? "")}
+	// 	ellipsize="3"
+	// 	max-width-chars="50"
+	// />
+	//   </box>
 }
 
--- config/ags/widget/Workspaces.tsx
+++ config/ags/widget/Workspaces.tsx
@@ -1,21 +1,23 @@
 import { bind } from "astal"
-import Hyprland from "gi://AstalHyprland"
+// import Hyprland from "gi://AstalHyprland"
+// import Niri from "gi://AstalNiri"
 
 export function Workspaces() {
-    const hypr = Hyprland.get_default()
-
-    return <box className="Workspaces">
-        {bind(hypr, "workspaces").as(wss => wss
-            .sort((a, b) => a.id - b.id)
-            .map(ws => (
-                <button
-                    className={bind(hypr, "focusedWorkspace").as(fw =>
-                        ws === fw ? "focused" : "")}
-                    onClicked={() => ws.focus()}>
-                    {ws.id}
-                </button>
-            ))
-        )}
-    </box>
+	// const hypr = Hyprland.get_default()
+	// const niri = Niri.get_default()
+	//
+	// return <box className="Workspaces">
+	// 	{bind(niri, "workspaces").as(wss => wss
+	// 		.sort((a, b) => a.id - b.id)
+	// 		.map(ws => (
+	// 			<button
+	// 				className={bind(hypr, "focusedWorkspace").as(fw =>
+	// 					ws === fw ? "focused" : "")}
+	// 				onClicked={() => ws.focus()}>
+	// 				{ws.id}
+	// 			</button>
+	// 		))
+	// 	)}
+	// </box>
 }
 
